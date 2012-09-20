d32729f55e39adda35430d308a775f8cf691deb8
m2m tech <tech@m2m.at>
2012-09-20 16:19:33 +0200

    initial commit

---
+++ Yahtzee.java
@@ -0,0 +1,77 @@
+/*
+ * File: Yahtzee.java
+ * ------------------
+ * This program will eventually play the Yahtzee game.
+ */
+
+import acm.io.*;
+import acm.program.*;
+import acm.util.*;
+
+public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
+	public void run() {
+		setupPlayers();
+		initDisplay();
+		playGame();
+	}
+	
+	/**
+	 * Prompts the user for information about the number of players, then sets up the
+	 * players array and number of players.
+	 */
+	private void setupPlayers() {
+		nPlayers = chooseNumberOfPlayers();	
+		
+		/* Set up the players array by reading names for each player. */
+		playerNames = new String[nPlayers];
+		for (int i = 0; i < nPlayers; i++) {
+			/* IODialog is a class that allows us to prompt the user for information as a
+			 * series of dialog boxes.  We will use this here to read player names.
+			 */
+			IODialog dialog = getDialog();
+			playerNames[i] = dialog.readLine("Enter name for player " + (i + 1));
+		}
+	}
+	
+	/**
+	 * Prompts the user for a number of players in this game, reprompting until the user
+	 * enters a valid number.
+	 * 
+	 * @return The number of players in this game.
+	 */
+	private int chooseNumberOfPlayers() {
+		/* See setupPlayers() for more details on how IODialog works. */
+		IODialog dialog = getDialog();
+		
+		while (true) {
+			/* Prompt the user for a number of players. */
+			int result = dialog.readInt("Enter number of players");
+			
+			/* If the result is valid, return it. */
+			if (result > 0 && result <= MAX_PLAYERS)
+				return result;
+			
+			dialog.println("Please enter a valid number of players.");
+		}
+	}
+	
+	/**
+	 * Sets up the YahtzeeDisplay associated with this game.
+	 */
+	private void initDisplay() {
+		display = new YahtzeeDisplay(getGCanvas(), playerNames);
+	}
+
+	/**
+	 * Actually plays a game of Yahtzee.  This is where you should begin writing your
+	 * implementation.
+	 */
+	private void playGame() {
+		/* You fill this in! */
+	}
+		
+	/* Private instance variables */
+	private int nPlayers;
+	private String[] playerNames;
+	private YahtzeeDisplay display;
+}
